edd85be8a10383a6da224f5fc08499a71b872821
Raketa <detroi.vv@gmail.com>
2016-12-22 22:42:19 +0200

    Маппинг Класса @Entity @Table(name = "employee")  @Id    @Column(name = "id")      @Column(name = "phone_number")       private String phoneNumber;    @Enumerated(EnumType.STRING)       @Column(name = "position")

---
+++ src/main/java/ua/goit/java/jdbc/Main.java
@@ -0,0 +1,7 @@
+package main.java.ua.goit.java.jdbc;
+
+/**
+ * Created by Raketa on 22.12.2016.
+ */
+public class Main {
+}
---
+++ src/main/java/ua/goit/java/jdbc/model/Position.java
@@ -0,0 +1,11 @@
+package main.java.ua.goit.java.jdbc.model;
+
+/**
+ * Created by Raketa on 22.12.2016.
+ */
+// 2. создадим enum
+public enum Position {
+    WAITER,
+    COOK,
+    MANAGER
+}
